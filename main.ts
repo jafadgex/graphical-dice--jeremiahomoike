82af29f1eccefb31f2ae5e466dd00f885d7bd02f
jafadgex <92938346+jafadgex@users.noreply.github.com>
2021-10-26 14:20:55 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+let set_number = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    set_number = randint(1, 6)
+    if (set_number == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (set_number == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+    } else if (set_number == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (set_number == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (set_number == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (set_number == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    } else {
+    	
+    }
+})
